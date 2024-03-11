a3b785f204e35421ad2bf1e66a77d3a15314c7f6
idrisGadi <idris.gadi@appliedaiconsulting.com>
2024-03-11 15:58:50 +0530

    refactor: ComboBox to Listbox

--- src/features/exchange/components/CurrenciesSelect.tsx
+++ src/features/exchange/components/CurrenciesSelect.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment, Dispatch, SetStateAction } from 'react';
-import { Combobox, Transition } from '@headlessui/react';
+import { Transition, Listbox } from '@headlessui/react';
 import { ChevronUpDownIcon, CheckIcon } from '@heroicons/react/20/solid';
 import { currencies } from '../utils/currencies';
 import { Currency } from '../types';
@@ -12,33 +12,33 @@ interface CurrenciesSelectProp {
 export const CurrenciesSelect: React.FC<CurrenciesSelectProp> = ({ selected, setSelected }) => {
   return (
     <div className='w-full max-w-screen-md'>
-      <Combobox
+      <Listbox
         value={selected}
         onChange={(currency) => setSelected(currency)}
+        defaultValue={selected}
         multiple
       >
         <div className='relative'>
-          <div className='relative min-h-12 w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm'>
-            <Combobox.Input
-              className='min-h-12 w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0'
-              displayValue={(currency: Currency[]) => currency.map((cur) => cur.code).join(', ')}
-            />
-            <Combobox.Button className='absolute inset-y-0 right-0 flex items-center pr-2'>
+          <Listbox.Button className='relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm'>
+            <span className='block truncate'>
+              {selected.map((curreny) => curreny.code).join(', ')}
+            </span>
+            <span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'>
               <ChevronUpDownIcon
                 className='h-5 w-5 text-gray-400'
                 aria-hidden='true'
               />
-            </Combobox.Button>
-          </div>
+            </span>
+          </Listbox.Button>
           <Transition
             as={Fragment}
             leave='transition ease-in duration-100'
             leaveFrom='opacity-100'
             leaveTo='opacity-0'
           >
-            <Combobox.Options className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm'>
+            <Listbox.Options className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm'>
               {currencies.map((currency) => (
-                <Combobox.Option
+                <Listbox.Option
                   key={currency.code}
                   className={({ active }) =>
                     `relative cursor-default select-none py-2 pl-10 pr-4 ${
@@ -47,7 +47,7 @@ export const CurrenciesSelect: React.FC<CurrenciesSelectProp> = ({ selected, set
                   }
                   value={currency}
                 >
-                  {({ selected, active }) => (
+                  {({ active, selected }) => (
                     <>
                       <span
                         className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}
@@ -68,12 +68,12 @@ export const CurrenciesSelect: React.FC<CurrenciesSelectProp> = ({ selected, set
                       ) : null}
                     </>
                   )}
-                </Combobox.Option>
+                </Listbox.Option>
               ))}
-            </Combobox.Options>
+            </Listbox.Options>
           </Transition>
         </div>
-      </Combobox>
+      </Listbox>
     </div>
   );
 };
